4355c97d8aef7f02c691dc7ab08abf7c8ba61aaf
Ramon R. <ramoncreepy@hotmail.com>
2024-09-22 11:54:32 -0300

    Adiciona registro de 'Hello World' no myApp.js

--- myApp.js
+++ myApp.js
@@ -1,38 +1,16 @@
 let express = require('express');
 let app = express();
+const PORT = 3000;
 
+// Rota básica
+app.get('/', (req, res) => {
+    console.log("Hello World");
+    res.send('Hello World');
+});
 
-console.log("Hello World");
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
